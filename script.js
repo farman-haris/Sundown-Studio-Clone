a85d1b97aca87d6305dafbd644e429943dcce43a
Farman Haris <farmanharis66@gmail.com>
2024-02-04 18:13:46 +0500

    Page 3 Completed

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
+// Loco Motive Scripting
+
 const scroll = new LocomotiveScroll({
-    el: document.querySelector('.main'),
-    smooth: true
+  el: document.querySelector(".main"),
+  smooth: true,
 });
+
+// Cards Scripting
+
+let elemC = document.querySelector(".elem-container");
+let fixed = document.querySelector(".fixed-image");
+elemC.addEventListener("mouseenter", function () {
+  fixed.style.display = "block";
+});
+elemC.addEventListener("mouseleave", function () {
+  fixed.style.display = "none";
+});
+
+let elems = document.querySelectorAll(".elem")
+elems.forEach((e) => {
+    e.addEventListener("mouseenter", () => {
+        let image = e.getAttribute("data-image")
+        fixed.style.backgroundImage = `url(${image})`
+    })
+})
